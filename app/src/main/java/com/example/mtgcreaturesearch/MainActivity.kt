27c450c5f2f756f9c8aa37b289efd46a8718359f
Pyrolin <mads.hr02@gmail.com>
2023-10-23 20:14:04 +0200

    Fixed text allignment

--- app/src/main/java/com/example/mtgcreaturesearch/MainActivity.kt
+++ app/src/main/java/com/example/mtgcreaturesearch/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,14 +50,20 @@ fun FavoritesColumn() {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "MTG Card Organizer",
-            modifier = Modifier.padding(16.dp)
-        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "MTG Card Organizer",
+            )
+        }
 
         Row(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .padding(16.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
