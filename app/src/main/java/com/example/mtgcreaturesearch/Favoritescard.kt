cb014c5232b6c76bf57308754c39e7a8eea00685
ACE1960 <113360654+ACE1960@users.noreply.github.com>
2023-11-09 12:16:58 +0100

    have ceated a Favoritescard

---
+++ app/src/main/java/com/example/mtgcreaturesearch/Favoritescard.kt
@@ -0,0 +1,27 @@
+package com.example.mtgcreaturesearch
+
+class Favoritescard {
+    private String cardName;
+    private String imageUrl;
+// Getter for cardName
+    public String getCardName();{
+        return cardName;
+    }
+// Setter for cardName
+    public void setCardName(String cardName){
+        this.cardName = cardName;
+    }
+// Getter for imageUrl
+    public String getImageUrl(){
+        return imageUrl;
+    }
+// Setter for imageUrl
+    public void setImageUrl(String imageUrl){
+        this.imageUrl = imageUrl;
+    }
+}
+
+
+
+
+}
