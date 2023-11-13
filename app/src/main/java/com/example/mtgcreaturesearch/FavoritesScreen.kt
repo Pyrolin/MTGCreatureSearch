82a898a0150b38594eddd6241618affae339f4f8
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-11-13 11:05:39 +0100

    Copied code over from browse and removed search bar

--- app/src/main/java/com/example/mtgcreaturesearch/FavoritesScreen.kt
+++ app/src/main/java/com/example/mtgcreaturesearch/FavoritesScreen.kt
@@ -1,14 +1,58 @@
 package com.example.mtgcreaturesearch
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mtgcreaturesearch.ui.theme.MTGCreatureSearchTheme
 
+val FavoriteExample = listOf("Card1", "Card2", "Card3")
 @Composable
 fun FavoritesScreen() {
-    Text(
-        text = "Hello",
+    Column(
         modifier = Modifier.fillMaxSize()
-    )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Title(name = "MTG Card Organizer")
+        }
+
+        // Add a divider bar under the title
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(Color.Gray)
+        )
+
+        Box(
+            modifier = Modifier
+                .padding(5.dp)
+        ) {
+            CardGrid(cards = FavoriteExample)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FavoriteScreenPreview() {
+    MTGCreatureSearchTheme {
+        BrowseScreen()
+    }
 }
