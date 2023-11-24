de88e2042195709d55aebb74115d22e459bcc9d7
Mads Hauberg <mads095h@gmail.com>
2023-11-24 09:58:38 +0100

    restructured cucumber tests and fixed the runner

--- app/src/main/java/com/example/mtgcreaturesearch/test/RunCucumberTest.kt
+++ app/src/main/java/com/example/mtgcreaturesearch/test/RunCucumberTest.kt
@@ -3,7 +3,8 @@ package com.example.mtgcreaturesearch.test
 import io.cucumber.junit.CucumberOptions
 import io.cucumber.junit.Cucumber
 import org.junit.runner.RunWith
-
 @RunWith(Cucumber::class)
-@CucumberOptions(plugin = ["pretty"])
+@CucumberOptions(
+    plugin = ["pretty", "html:target/cucumber-report.html"],
+    features = ["src/main/res/com/example/mtgcreaturesearch/test"])
 class RunCucumberTest
