5fe94e77b6b722406fb625c0f68a522a3b36904d
Mads Hauberg <mads095h@gmail.com>
2023-11-30 14:33:31 +0100

    Finished image fetch test and made a favorites test

--- app/src/test/java/com/example/mtgcreaturesearch/StepDefs.kt
+++ app/src/test/java/com/example/mtgcreaturesearch/StepDefs.kt
@@ -1,28 +1,53 @@
 package com.example.mtgcreaturesearch
 
+import CardApi
+import com.example.mtgcreaturesearch.Model.Data
 import io.cucumber.java.PendingException
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
-
+import kotlinx.coroutines.*
+import kotlin.system.*
 
 class StepDefs {
+    lateinit var listResult: List<Data>
+    lateinit var imageUrl: String
 
-    @Given("I want to browse creature")
-    fun api_has_a_creature() {
-        // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
+    lateinit var card: Data
+
+    var favorites: ArrayList<String> = ArrayList()
+
+    @Given("Cards are visible")
+    fun api_has_a_creature() = runBlocking {
+        listResult = getCratures()
+        card = listResult[0]
     }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @When("I want to see the image")
     fun i_want_to_get_the_image() {
-        // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
+        imageUrl = card.image_uris.png
     }
 
-    @Then("I should be given the image url")
-    fun i_should_be_given_the_image_url() {
-        // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
+    @When("I want to favorite the card")
+    fun i_want_to_favorite_the_card() {
+        favorites.add(card.id)
     }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Then("I should be given the image url {string}")
+    fun i_should_be_given_the_image_url(url: String) {
+        assert(card.image_uris.png == url)
+    }
+
+    @Then("the card should be favorited")
+    fun the_card_should_be_favorited() {
+        assert(favorites.contains(card.id))
+    }
+}
+
+suspend fun getCratures(): List<Data> {
+    return CardApi.retrofitService.getPhotos().data
 }
--- app/src/test/res/can_image_get_fetched.feature
+++ app/src/test/res/can_image_get_fetched.feature
@@ -2,6 +2,6 @@ Feature: Can we fetch the image of cards?
   We want to see the card images
 
   Scenario: Image is shown
-    Given I want to browse creature
+    Given Cards are visible
     When I want to see the image
-    Then I should be given the image url
+    Then I should be given the image url "https://cards.scryfall.io/png/front/0/e/0ed089db-54b9-40d7-9a91-19f0bab44303.png?1690005365"
