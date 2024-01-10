fdb64e35fcf7b967dfaea1a86d49eef3be042ff3
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2024-01-10 16:37:31 +0100

    back botton on big card

--- app/src/main/java/com/example/mtgcreaturesearch/View/CardScreen.kt
+++ app/src/main/java/com/example/mtgcreaturesearch/View/CardScreen.kt
@@ -1,9 +1,11 @@
 package com.example.mtgcreaturesearch.View
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -26,52 +29,73 @@ import com.example.mtgcreaturesearch.ViewModel.CardViewModel
 fun CardScreen(cardViewModel: CardViewModel = viewModel(), navController: NavController, card: ShownCards) {
     Box(modifier = Modifier.fillMaxSize()) {
         Card(cardViewModel, navController, card, setonClick = false)
-    }
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(55.dp)
-    ) {
-        // BottomTabBar Background
-        Image(
-            painter = painterResource(id = R.drawable.favorites_tabbar_background),
-            contentDescription = "Tab Bar Background",
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
-        )
-
-        Row(
+        Column {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                contentAlignment = Alignment.TopStart
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.backspace),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(30.dp)
+                        .background(Color.Transparent)
+                        .clickable {
+                            navController.navigate("HomeScreen")
+                        }
+                )
+            }
+        }
+        Box(
             modifier = Modifier
+                .align(Alignment.BottomCenter)
                 .fillMaxWidth()
-                .height(70.dp)
-                .padding(top = 10.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
+                .height(55.dp)
         ) {
-            // Home Tab
+            // BottomTabBar Background
             Image(
-                painter = painterResource(id = R.drawable.burgermenu_hvid),
-                contentDescription = "Home",
-                modifier = Modifier
-                    .size(30.dp)
-                    .clickable { navController.navigate("homeScreen") }
+                painter = painterResource(id = R.drawable.favorites_tabbar_background),
+                contentDescription = "Tab Bar Background",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
             )
 
-            // Browse Tab
-            Image(
-                painter = painterResource(id = R.drawable.search),
-                contentDescription = "Browse",
+            Row(
                 modifier = Modifier
-                    .size(30.dp)
-                    .clickable { navController.navigate("browseScreen") }
-            )
+                    .fillMaxWidth()
+                    .height(70.dp)
+                    .padding(top = 10.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // Home Tab
+                Image(
+                    painter = painterResource(id = R.drawable.burgermenu_hvid),
+                    contentDescription = "Home",
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clickable { navController.navigate("homeScreen") }
+                )
 
-            // Favorites Tab
-            Image(
-                painter = painterResource(id = R.drawable.favorite_hvid),
-                contentDescription = "Favorites",
-                modifier = Modifier.size(30.dp) // Not clickable since it's the current screen
-            )
+                // Browse Tab
+                Image(
+                    painter = painterResource(id = R.drawable.search),
+                    contentDescription = "Browse",
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clickable { navController.navigate("browseScreen") }
+                )
+
+                // Favorites Tab
+                Image(
+                    painter = painterResource(id = R.drawable.favorite_hvid),
+                    contentDescription = "Favorites",
+                    modifier = Modifier.size(30.dp) // Not clickable since it's the current screen
+                )
+            }
         }
     }
+
 }
